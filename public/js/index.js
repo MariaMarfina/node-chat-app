6cf0d406c456a91f94814d7238cd7883619ad939
Maria <maria.marfina1@gmail.com>
2018-03-06 15:48:48 +0300

    Init commit

--- public/js/index.js
+++ public/js/index.js
@@ -11,7 +11,3 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
     console.log('New message', message);
 });
-
-// 2 events -
-// newMessage(from server, params: from, text, createdAt. Got new message and object print in the browser) and
-// createMessage, data: from, text
