4d88a4296f4a7f8d2937596c41e446669848978e
Maria <example@example.com>
2018-03-06 15:11:37 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Maria',
-        text: 'Hooray-hooray'
-    });
 });
 
 socket.on('disconnect', function () {
