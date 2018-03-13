0493a2107eaed2883d23e6b6bdf85a6e51e0fe65
Maria <maria.marfina1@gmail.com>
2018-03-13 22:12:31 +0300

    Add mustache.js for message templates

--- server/server.js
+++ server/server.js
@@ -15,9 +15,8 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    //socket.emit to a user who joined from admin: welcome to the chat app
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-    //socket.broadcast.emit from admin: New user joined
+
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (message, callback) => {
